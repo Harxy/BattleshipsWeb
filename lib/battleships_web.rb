128727034db770b76f1a0cf679891428c95f6403
Natalia Antonova <5555482@gmail.com>
2015-06-22 14:05:05 +0100

    index file

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -4,6 +4,7 @@ class BattleshipsWeb < Sinatra::Base
   get '/' do
     'Hello BattleshipsWeb!'
   end
+  set :views, proc { File.join(root, '..', 'views') }
 
   # start the server if ruby file executed directly
   run! if app_file == $0
