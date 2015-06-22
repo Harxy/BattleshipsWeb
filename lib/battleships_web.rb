d818eaa73abc1ccd777a4c22167339856d1b4236
TheoLeanse <theoleanse@gmail.com>
2015-06-22 16:37:26 +0100

    uses the board deisplay from Ben's Battleships gem

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,17 +1,22 @@
 require 'sinatra/base'
+require 'battleships'
 
 class BattleshipsWeb < Sinatra::Base
   get '/' do
     erb :index
   end
+
   set :views, proc { File.join(root, '..', 'views') }
+
   get '/name_entry' do
     @visitor = params[:name]
     erb :name_entry
   end
 
   get '/board' do
-
+    game = Game.new Player, Board
+    game.player_1.place_ship Ship.battleship, :A1, :vertically
+    @board = game.opponent_board_view game.player_1
     erb :board
   end
 
